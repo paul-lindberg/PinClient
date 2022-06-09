b2b6692ba469cac922b14e40a4805111c7dccdb7
paul-lindberg <paul.lindberg@mymail.champlain.edu>
2022-06-09 13:51:57 -0500

    add ip of current computer

--- PinClient/TcpTimeClient.cs
+++ PinClient/TcpTimeClient.cs
@@ -11,7 +11,7 @@ namespace PinClient
     public class TcpTimeClient
     {
         private const int portNum = 13;
-        private const string hostName = "localhost";//"host.contoso.com";
+        private const string hostName = "192.168.0.122";//"host.contoso.com";
 
         public static int Init(String[] args)
         {
