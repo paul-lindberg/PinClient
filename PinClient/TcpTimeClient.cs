2350410a085b98b938830c34fb4846667f65f0ed
paul-lindberg <paul.lindberg@mymail.champlain.edu>
2022-06-09 13:38:13 -0500

    Add time client project, run in program.cs

---
+++ PinClient/TcpTimeClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace PinClient
+{
+
+    public class TcpTimeClient
+    {
+        private const int portNum = 13;
+        private const string hostName = "localhost";//"host.contoso.com";
+
+        public static int Init(String[] args)
+        {
+            try
+            {
+                var client = new TcpClient(hostName, portNum);
+
+                NetworkStream ns = client.GetStream();
+
+                byte[] bytes = new byte[1024];
+                int bytesRead = ns.Read(bytes, 0, bytes.Length);
+
+                Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return 0;
+        }
+    }
+}
